0bdd0db16e34c35355ca0d83d65ad47d9c4b16d3
Daniel Compton <desk@danielcompton.net>
2015-12-29 14:57:25 +1300

    Update git aliases and gitignore to match my preferences

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,13 +7,26 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
+alias gl='git pull --ff-only --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gp='git push'
 alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
+alias gc='git commit --verbose'
+alias gca='git commit --amend --verbose'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+
+alias gap="git add --patch"
+alias gdt="git difftool"
+alias gcm="git commit -m"
+alias gmt="git mergetool"
+alias grhh="git reset HEAD --hard"
+alias grb="git rebase"
+alias grbc="git rebase --continue"
+alias gst="git stash"
+alias gstp="git stash pop"
+alias ga.="git add ."
+alias ga="git add"
+alias gcl="git clone"
