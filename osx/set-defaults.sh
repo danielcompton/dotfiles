89953c66a78c45d00a49854381b2ab6530f9c594
Daniel Compton <desk@danielcompton.net>
2016-09-01 20:49:46 +1200

    Set key repeat

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -20,7 +20,7 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 chflags nohidden ~/Library
 
 # Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain KeyRepeat -int 2
 
 # Disable local Time Machine snapshots
 sudo tmutil disablelocal
