129ebf13ac0f0404bf540e23d6051bc4be9f490d
Daniel Compton <desk@danielcompton.net>
2016-09-04 15:36:35 +1200

    Updates

--- clojure/aliases.fish
+++ clojure/aliases.fish
@@ -4,7 +4,7 @@ function dirac
   /Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary \
                                    --remote-debugging-port=9222 \
                                    --no-first-run \
-                                   --user-data-dir=/Users/danielcompton/cd.dirac-chrome-profile ^ /dev/null ^^/dev/null &
+                                   --user-data-dir=/Users/danielcompton/.dirac-chrome-profile ^ /dev/null ^^/dev/null &
 end
 
 # alias dirac="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --remote-debugging-port=9222 --no-first-run  --user-data-dir=~/.dirac-chrome-profile"
