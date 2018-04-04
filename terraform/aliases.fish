facda7cd03f5456563a8f4e346286b5ac6cc786c
Daniel Compton <desk@danielcompton.net>
2018-04-05 09:15:31 +1200

    More changes

--- terraform/aliases.fish
+++ terraform/aliases.fish
@@ -1,3 +1,4 @@
+# https://news.ycombinator.com/item?id=14540986
 alias tfplan='terraform plan -out=.tfplan -refresh=false'
 alias tffreshplan='terraform plan -out=.tfplan'
 alias tfapply='terraform apply .tfplan; rm .tfplan'
