3eb959ba8d37f10a676100453ce157a35d3347ec
Daniel Compton <desk@danielcompton.net>
2018-09-24 08:51:49 +1200

    Set vscode as default dotfiles editor

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,6 +1,6 @@
 alias reload!='. ~/.zshrc'
 alias dotf='cd ~/.dotfiles'
-alias mated='atom ~/.dotfiles/'
+alias mated='code ~/.dotfiles/'
 alias flushdns='sudo killall -HUP mDNSResponder'
 alias flushhsts='launchctl unload /System/Library/LaunchAgents/com.apple.nsurlstoraged.plist; and sudo killall nsurlstoraged; rm -f ~/Library/Cookies/HSTS.plist; and launchctl load /System/Library/LaunchAgents/com.apple.nsurlstoraged.plist; echo "Empty Caches in Safari too, http->https redirects are cached."'
 
