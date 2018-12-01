048bc115ae4ee4a607ad65b8b1b219eb71f99d0d
danielcompton <desk@danielcompton.net>
2018-12-01 13:50:35 +1300

    Add bat

--- system/env.fish
+++ system/env.fish
@@ -1,5 +1,7 @@
 export EDITOR='vim'
 
+export BAT_PAGER="less -R" # https://github.com/sharkdp/bat#using-a-different-pager
+
 # http://unix.stackexchange.com/questions/197839/why-does-exporting-vim-as-editor-in-zsh-disable-keyboard-shortcuts
 # TODO: figure out how to set this in fish 2.3.1
 # set -o emacs
