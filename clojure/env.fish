13a552a057d7a944f4d2241786cdb6aa63a7026d
Daniel Compton <desk@danielcompton.net>
2018-12-03 09:58:56 +1300

    Set Java home

--- clojure/env.fish
+++ clojure/env.fish
@@ -1,8 +1,8 @@
 # For ClojureScript development:
 
 # Need to set this here, as this env.fish file is run before the Java one.
-set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
-#set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_162.jdk/Contents/Home"
+# set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
+set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home"
 
 set -x SPIDERMONKEY_HOME "$HOME/Developer/js-engines/jsshell-mac"
 set -x NASHORN_HOME "$JAVA_HOME/bin"
