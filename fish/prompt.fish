026cd1db478b737ee0cf485e46dc0172745bdd36
Daniel Compton <desk@danielcompton.net>
2019-09-06 20:42:18 +1200

    Update network debugging commands

--- fish/prompt.fish
+++ fish/prompt.fish
@@ -1,8 +1,14 @@
 set -g -x PATH ~/bin $PATH
 set -g -x fish_greeting ''
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+test -e {$HOME}/.iterm2_shell_integration.fish
+and source {$HOME}/.iterm2_shell_integration.fish
 
 function pport
-  lsof -n -i:$argv[1] | grep LISTEN
+    lsof -n -i :$argv[1]
+end
+
+function nport
+    # macOS Awk is missing word boundaries so we just match on whitespace
+    netstat -nt | awk "/\.$argv[1] / || NR <= 2"
 end
